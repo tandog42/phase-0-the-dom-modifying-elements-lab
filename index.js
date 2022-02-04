044a758f29bdfa411d74073034d37c461e252ccb
Tanner <troxs690@gmail.com>
2022-02-04 15:54:37 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const Hell = document.querySelector('.main')
+const del = main.remove('Hell')
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Tanner is the champion'
+
